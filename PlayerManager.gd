15f1e5fdcb9becffc5f76e24716e15b633cdca55
swomfire <vucuongJim@gmail.com>
2025-04-16 16:59:27 +0700

    add multi select units

--- PlayerManager.gd
+++ PlayerManager.gd
@@ -1,40 +1,75 @@
-extends Node
+extends Node2D
 
-# The currently selected unit (could be a custom Unit class extending Node2D or similar)
-var selected_node: Node2D
+var selected_nodes: Dictionary
 var hover_node: Node2D
-
+var is_multi_select := false
 var deploy_unit = preload("res://unit.tscn")
 
-# set unit destination
+var start_pos: Vector2
+var end_pos: Vector2
+var selecting := false
+var selected_rect := Rect2()
 
 func _hover(node: Node2D) -> void:
-	if !node:
-		return
-	
-	if (node is Unit and hover_node is Caslte) or hover_node == null:
+	if node and ((node is Unit and hover_node is Caslte) or hover_node == null):
 		hover_node = node
-		
+
 func _dehover(node: Node2D) -> void:
 	if hover_node == node:
 		hover_node = null
-		
 
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-		if selected_node:
-			var destination = get_viewport().get_mouse_position()
-			selected_node.properties["destination"] = destination
+	is_multi_select = Input.is_key_pressed(KEY_SHIFT)
+
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_LEFT:
+				if event.pressed and hover_node is Unit:
+					if is_multi_select:
+						if selected_nodes.has(hover_node):
+							selected_nodes.erase(hover_node)
+						else:
+							selected_nodes[hover_node] = true
+					else:
+						selected_nodes = {hover_node: true}
+					return
+				elif event.pressed and hover_node is Caslte and deploy_unit:
+					var spawn = deploy_unit.instantiate()
+					spawn.unit_id = "unit_00" + str(UnitManager.last_unit_index + 1)
+					spawn.position = hover_node.position
+					get_tree().root.add_child(spawn)
+					return
+			
+				if event.pressed:
+					start_pos = get_viewport().get_mouse_position()
+					selecting = true
+				elif selecting:
+					end_pos = get_viewport().get_mouse_position()
+					selecting = false
+					selected_rect = Rect2(start_pos, end_pos - start_pos).abs()
+					_select_units_in_rect(selected_rect)
+					return
+
+			MOUSE_BUTTON_RIGHT:
+				if event.pressed and selected_nodes:
+					var destination = get_viewport().get_mouse_position()
+					for node in selected_nodes:
+						if node:
+							node.properties["destination"] = destination
+
+func _process(delta):
+	queue_redraw()
+
+func _draw():
+	if selecting:
+		var rect = Rect2(start_pos, get_viewport().get_mouse_position() - start_pos).abs()
+		draw_rect(rect, Color(0, 0.5, 1, 0.3), true)
+		draw_rect(rect, Color(0, 0.5, 1), false)
+
+func _select_units_in_rect(rect: Rect2):
+	if !is_multi_select:
+		selected_nodes.clear()
 	
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		if hover_node is Unit:
-			print("select unit")
-			selected_node=hover_node
-		elif hover_node is Caslte and deploy_unit != null:
-			print("select castle")
-			var 	spawn = deploy_unit.instantiate()
-			spawn.unit_id = "unit_00" + str(UnitManager.last_unit_index+1)
-			spawn.position = hover_node.position
-			get_tree().root.add_child(spawn)
-		else:
-			selected_node=null
+	for unit in UnitManager.registered_units.values():
+		if rect.has_point(unit.global_position):
+			selected_nodes[unit] = true
--- Selectable.gd
+++ Selectable.gd
@@ -8,7 +8,7 @@ func _ready():
 	self.mouse_exited.connect(_on_mouse_exited)
 	
 func _process(delta: float) -> void:
-	if PlayerManager.selected_node is Unit and PlayerManager.selected_node.unit_id == unit.unit_id:
+	if PlayerManager.selected_nodes.has(unit):
 		sprite.modulate = Color(0.5, 1, 0.5)  # Green tint
 	elif PlayerManager.hover_node is Unit and PlayerManager.hover_node.unit_id == unit.unit_id:
 		sprite.modulate = Color(1, 1, 0.5)  # Yellow tint
