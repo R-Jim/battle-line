4ccf209ea61a77e7ca03f6ce75537d832818c7b5
swomfire <vucuongJim@gmail.com>
2025-05-22 16:39:51 +0700

    Add player healing camp

--- Effect/OverworldEffectZone.gd
+++ Effect/OverworldEffectZone.gd
@@ -1,20 +1,20 @@
 extends Area2D
 
 
+@export var _faction: int
 var effects: Array[SquadEffect] = []
 var effected_node: Dictionary[Squad, Array] = {}
 
 func _ready():
-    self.area_entered.connect(_on_area_entered)
-    self.area_exited.connect(_on_area_exited)
+    self.body_entered.connect(_on_body_entered)
+    self.body_exited.connect(_on_body_exited)
     
     for child in get_children():
         if child is SquadEffect:
             effects.append(child)
 
 
-func _on_area_entered(area: Area2D):
-    var node = area.get_parent()
+func _on_body_entered(node: Node):
     if node is not Squad:
         return
     
@@ -22,20 +22,23 @@ func _on_area_entered(area: Area2D):
         return
     
     var squad: Squad = node
+    if squad.get_faction() != _faction:
+        return
+    print(squad.name)
     effected_node[squad] = []
     for effect in effects:
         var squad_effect = effect.duplicate()
         squad.add_effect(squad_effect)
-        effected_node[node].append(squad_effect)
+        effected_node[squad].append(squad_effect)
     
 
 
-func _on_area_exited(area: Area2D):
-    var node = area.get_parent()
+func _on_body_exited(node: Node):
     if node is not Squad:
         return
     
     var squad: Squad = node
+    print(squad.name, " exit")
     if  effected_node.has(squad):
         for effect in effected_node[squad]:
             effect.queue_free()
